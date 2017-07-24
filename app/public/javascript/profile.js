6dea83e77a7d9f1030a25c0b8821cb8ebbd5dc3b
kaylea <kayleabox09@gmail.com>
2017-07-23 22:51:45 -0500

    profile is kind of working!

--- app/public/javascript/profile.js
+++ app/public/javascript/profile.js
@@ -1,11 +1,18 @@
 var path = window.location.pathname;
 path = path.replace("/profile", "/api");
+var user = path.replace("/api/", "")
 console.log(path);
+$("#sign-in").append(user);
 
 $.get(path)
 .done(function (data) {
     console.log(data);
-    for(i=0; i<data.matches.length; i++){
-        $("#friend-peas").append("<p>"+data.matches[i]+"</p>");
+    if(data.matches){
+        for(i=0; i<data.matches.length; i++){
+            $("#friend-peas").append("<p>"+data.matches[i]+"</p>");
+        }
+    }
+    else{
+        $("#friend-peas").append("<p>You don't have any peas!</p>");
     }
 });
